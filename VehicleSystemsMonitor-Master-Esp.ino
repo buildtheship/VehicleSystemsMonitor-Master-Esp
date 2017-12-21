31ecfe18490a3b92fd9ec05692b2dcdbbc0507fb
Derrick- <derrick@alienseed.com>
2017-12-21 13:15:03 -0500

    EPD code from https://github.com/ZinggJM/GxEPD

--- VehicleSystemsMonitor-Master-Esp.ino
+++ VehicleSystemsMonitor-Master-Esp.ino
@@ -1,13 +1,149 @@
+
+// Supporting Arduino Forum Topics:
+// Waveshare e-paper displays with SPI: http://forum.arduino.cc/index.php?topic=487007.0
+// Good Dispay ePaper for Arduino : https://forum.arduino.cc/index.php?topic=436411.0
+
+// mapping suggestion from Waveshare Paper to Wemos D1 mini
+// BUSY -> D2, RST -> D4, DC -> D3, CS -> D8, CLK -> D5, DIN -> D7, GND -> GND, 3.3V -> 3.3V
+
+// mapping suggestion for ESP32, e.g. LOLIN32, see .../variants/.../pins_arduino.h for your board
+// NOTE: there are variants with different pins for SPI ! CHECK SPI PINS OF YOUR BOARD
+// BUSY -> 4, RST -> 16, DC -> 17, CS -> SS(5), CLK -> SCK(18), DIN -> MOSI(23), GND -> GND, 3.3V -> 3.3V
+
+// mapping suggestion for AVR, UNO, NANO etc.
+// BUSY -> 7, RST -> 9, DC -> 8, CS-> 10, CLK -> 13, DIN -> 11
+
+// include library, include base class, make path known
+#include <GxEPD.h>
+
+// select the display class to use, only one
+//#include <GxGDEP015OC1/GxGDEP015OC1.cpp>    // 1.54" b/w
+//#include <GxGDEW0154Z04/GxGDEW0154Z04.cpp>  // 1.54" b/w/r
+//#include <GxGDE0213B1/GxGDE0213B1.cpp>      // 2.13" b/w
+//#include <GxGDEW0213Z16/GxGDEW0213Z16.cpp>  // 2.13" b/w/r
+//#include <GxGDEH029A1/GxGDEH029A1.cpp>      // 2.9" b/w
+//#include <GxGDEW029Z10/GxGDEW029Z10.cpp>    // 2.9" b/w/r
+//#include <GxGDEW027C44/GxGDEW027C44.cpp>    // 2.7" b/w/r
+#include <GxGDEW042T2/GxGDEW042T2.cpp>      // 4.2" b/w
+//#include <GxGDEW075T8/GxGDEW075T8.cpp>      // 7.5" b/w
+//#include <GxGDEW075Z09/GxGDEW075Z09.cpp>    // 7.5" b/w/r
+
+// uncomment next line for drawBitmap() test
+#include GxEPD_BitmapExamples
+
+// FreeFonts from Adafruit_GFX
+#include <Fonts/FreeMonoBold9pt7b.h>
+#include <Fonts/FreeMonoBold12pt7b.h>
+#include <Fonts/FreeMonoBold18pt7b.h>
+#include <Fonts/FreeMonoBold24pt7b.h>
+
+#include <GxIO/GxIO_SPI/GxIO_SPI.cpp>
+#include <GxIO/GxIO.cpp>
+
+#if defined(ESP8266)
+
+// generic/common.h
+//static const uint8_t SS    = 15;
+//static const uint8_t MOSI  = 13;
+//static const uint8_t MISO  = 12;
+//static const uint8_t SCK   = 14;
+// pins_arduino.h
+//static const uint8_t D8   = 15;
+//static const uint8_t D7   = 13;
+//static const uint8_t D6   = 12;
+//static const uint8_t D5   = 14;
+
+// GxIO_SPI(SPIClass& spi, int8_t cs, int8_t dc, int8_t rst = -1, int8_t bl = -1);
+GxIO_Class io(SPI, SS, 0, 2); // arbitrary selection of D3(=0), D4(=2), selected for default of GxEPD_Class
+                              // GxGDEP015OC1(GxIO& io, uint8_t rst = 2, uint8_t busy = 4);
+GxEPD_Class display(io); // default selection of D4(=2), D2(=4)
+
+#elif defined(ESP32)
+
+// pins_arduino.h, e.g. LOLIN32
+//static const uint8_t SS    = 5;
+//static const uint8_t MOSI  = 23;
+//static const uint8_t MISO  = 19;
+//static const uint8_t SCK   = 18;
+
+// GxIO_SPI(SPIClass& spi, int8_t cs, int8_t dc, int8_t rst = -1, int8_t bl = -1);
+GxIO_Class io(SPI, SS, 17, 16); // arbitrary selection of 17, 16
+                                // GxGDEP015OC1(GxIO& io, uint8_t rst = D4, uint8_t busy = D2);
+GxEPD_Class display(io, 16, 4); // arbitrary selection of (16), 4
+
+#endif
+
+
 void setup()
 {
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println("setup");
 
-  /* add setup code here */
+    display.init();
 
+    Serial.println("setup done");
 }
 
 void loop()
 {
+    showBitmapExample();
+    drawCornerTest();
+    showFont("FreeMonoBold9pt7b", &FreeMonoBold9pt7b);
+    showFont("FreeMonoBold12pt7b", &FreeMonoBold12pt7b);
+    showFont("FreeMonoBold18pt7b", &FreeMonoBold18pt7b);
+    showFont("FreeMonoBold24pt7b", &FreeMonoBold24pt7b);
 
-  /* add main program code here */
+    delay(10000);
+}
 
+void showBitmapExample()
+{
+#ifdef _GxBitmapExamples_H_
+    display.drawExampleBitmap(BitmapExample1, sizeof(BitmapExample1));
+    delay(2000);
+    display.drawExampleBitmap(BitmapExample2, sizeof(BitmapExample2));
+    delay(2000);
+    display.setRotation(0);
+    display.fillScreen(GxEPD_WHITE);
+    display.drawExampleBitmap(BitmapExample1, 0, 0, GxEPD_WIDTH, GxEPD_HEIGHT, GxEPD_BLACK);
+    display.update();
+    delay(10000);
+#endif
+}
+
+void showFont(const char name[], const GFXfont* f)
+{
+    display.fillScreen(GxEPD_WHITE);
+    display.setTextColor(GxEPD_BLACK);
+    display.setFont(f);
+    display.setCursor(0, 0);
+    display.println();
+    display.println(name);
+    display.println(" !\"#$%&'()*+,-./");
+    display.println("0123456789:;<=>?");
+    display.println("@ABCDEFGHIJKLMNO");
+    display.println("PQRSTUVWXYZ[\\]^_");
+
+    display.println("`abcdefghijklmno");
+    display.println("pqrstuvwxyz{|}~ ");
+    display.update();
+    delay(5000);
+}
+
+void drawCornerTest()
+{
+    uint8_t rotation = display.getRotation();
+    for (uint16_t r = 0; r < 4; r++)
+    {
+        display.setRotation(r);
+        display.fillScreen(GxEPD_WHITE);
+        display.fillRect(0, 0, 8, 8, GxEPD_BLACK);
+        display.fillRect(display.width() - 18, 0, 16, 16, GxEPD_BLACK);
+        display.fillRect(display.width() - 25, display.height() - 25, 24, 24, GxEPD_BLACK);
+        display.fillRect(0, display.height() - 33, 32, 32, GxEPD_BLACK);
+        display.update();
+        delay(5000);
+    }
+    display.setRotation(rotation); // restore
 }
